c19f1b1315c7e344d66157c74b19900937a76aa6
ProgrammingHero <tamimhossain6421@gmail.com>
2023-12-21 10:32:36 +0600

    set up project

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+
+
+const MainLayout = () => {
+    return (
+        <div className="max-w-[1200px] mx-0">
+            <h1>This Is Main layout </h1>
+        </div>
+    );
+};
+
+export default MainLayout;
